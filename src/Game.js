c1ef90a3b886734a5672ef01beac988d2b657dc4
Hossein <mohammadhossein211@gmail.com>
2020-05-03 17:22:30 +0430

    Add github link

--- src/Game.js
+++ src/Game.js
@@ -1,7 +1,7 @@
-import React, { Component } from "react";
-import Dice from "./Dice";
-import ScoreTable from "./ScoreTable";
-import "./Game.css";
+import React, { Component } from 'react';
+import Dice from './Dice';
+import ScoreTable from './ScoreTable';
+import './Game.css';
 
 const NUM_DICE = 5;
 const NUM_ROLLS = 3;
@@ -27,8 +27,8 @@ class Game extends Component {
         smallStraight: undefined,
         largeStraight: undefined,
         yahtzee: undefined,
-        chance: undefined
-      }
+        chance: undefined,
+      },
     };
     console.log(this.state);
     this.roll = this.roll.bind(this);
@@ -48,45 +48,45 @@ class Game extends Component {
 
   roll(evt) {
     // roll dice whose indexes are in reroll
-    this.setState(st => ({
+    this.setState((st) => ({
       dice: st.dice.map((d, i) =>
         st.locked[i] ? d : Math.ceil(Math.random() * 6)
       ),
       locked: st.rollsLeft > 1 ? st.locked : Array(NUM_DICE).fill(true),
       rollsLeft: st.rollsLeft - 1,
-      rolling: false
+      rolling: false,
     }));
   }
 
   toggleLocked(idx) {
     // toggle whether idx is in locked or not
     if (this.state.rollsLeft > 0 && !this.state.rolling) {
-      this.setState(st => ({
+      this.setState((st) => ({
         locked: [
           ...st.locked.slice(0, idx),
           !st.locked[idx],
-          ...st.locked.slice(idx + 1)
-        ]
+          ...st.locked.slice(idx + 1),
+        ],
       }));
     }
   }
 
   doScore(rulename, ruleFn) {
     // evaluate this ruleFn with the dice and score this rulename
-    this.setState(st => ({
+    this.setState((st) => ({
       scores: { ...st.scores, [rulename]: ruleFn(this.state.dice) },
       rollsLeft: NUM_ROLLS,
-      locked: Array(NUM_DICE).fill(false)
+      locked: Array(NUM_DICE).fill(false),
     }));
     this.animateRoll();
   }
 
   displayRollInfo() {
     const messages = [
-      "0 Rolls Left",
-      "1 Roll Left",
-      "2 Rolls Left",
-      "Starting Round"
+      '0 Rolls Left',
+      '1 Roll Left',
+      '2 Rolls Left',
+      'Starting Round',
     ];
     return messages[this.state.rollsLeft];
   }
@@ -108,7 +108,7 @@ class Game extends Component {
             <div className='Game-button-wrapper'>
               <button
                 className='Game-reroll'
-                disabled={locked.every(x => x) || rollsLeft === 0 || rolling}
+                disabled={locked.every((x) => x) || rollsLeft === 0 || rolling}
                 onClick={this.animateRoll}
               >
                 {this.displayRollInfo()}
